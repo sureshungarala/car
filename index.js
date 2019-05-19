1aa1265865e4b6b80694b0867ffb94aede8f9c7c
Ungarala Suresh <iamuvvsuresh@gmail.com>
2019-05-19 16:30:08 +0530

    collision detection, score and reload

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 let canvas = document.getElementById('road'),
     context = canvas.getContext('2d');
-let brickInterval, latestBrickLevel = 0;
-const width = canvas.width, height = canvas.height, noOfLanes = 4;
+let brickInterval, latestBrickLevel = 0, initBrickMargin = 0, score = 0;
+let pauseInterval = false, gameLost = false;
+const width = canvas.width, height = canvas.height, noOfLanes = 4, carMargin = 25, carPadding = 5;
 const car = {
     img: document.createElement("img"),
     width: 0,
     height: 0,
     position: {
-        x: 25,
-        y: height - 25
+        x: carMargin,
+        y: height - carMargin
     }
 },
     brick = {
         img: document.createElement('img'),
         width: 0,
-        height: 0,
-        position: {
-            x: 25,
-            y: 25
-        }
+        height: 0
     };
 car.img.src = 'assets/car.png';
 brick.img.src = 'assets/brick.png';
@@ -32,23 +29,30 @@ window.onload = function () {
     drawCar();
     brick.width = car.width;
     brick.height = car.width;
+    let jumpPixels = (height - carMargin - carPadding) / (car.height + brick.height);
+    initBrickMargin = (car.height + brick.height) * (jumpPixels - Math.floor(jumpPixels));
     blocklanes();
-    console.log(width, height, car.width, car.height, brick.width, brick.height);
 }
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'Right' || event.key === 'ArrowRight') {
         if (car.position.x + 100 < width) {
+            pauseInterval = true;
             car.position.x += 100;
             draw();
+            pauseInterval = false;
         }
         console.log('right key');
     } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
         if (car.position.x - 100 > 0) {
+            pauseInterval = true;
             car.position.x -= 100;
             draw();
+            pauseInterval = false;
         }
         console.log('left key');
+    } else if (gameLost && (event.key === ' ' || event.code === 'Space')) {
+        document.location.reload();
     }
 });
 
@@ -56,6 +60,7 @@ function draw() {
     context.clearRect(0, 0, width, height);
     drawCar();
     drawBricks();
+    drawScore();
 }
 function drawCar() {
     context.drawImage(car.img, car.position.x, car.position.y, car.width, car.height);
@@ -63,26 +68,43 @@ function drawCar() {
 function drawBricks() {
     for (let lane of lanes) {
         for (let block of lane) {
-            let levelDiff = latestBrickLevel - block.brickLevel,
-                blockY = levelDiff === 0 ? 25 : levelDiff * (car.height + brick.width) + 25;
+            let blockY = (latestBrickLevel - block.brickLevel) * (car.height + brick.height) + initBrickMargin;
             if (blockY < height) {
+                blockY = (blockY > car.position.y && blockY + 2 * carPadding < height) ? blockY + 2 * carPadding : blockY;
+                if (blockY > car.position.y + car.height && !pauseInterval) {
+                    score++;
+                    if (block.x === car.position.x) {
+                        gameLost = true;
+                        cancelInterval();
+                        alert('GAME OVER!');
+                    }
+                }
                 context.drawImage(brick.img, block.x, blockY, brick.width, brick.height);
             }
         }
     }
 }
+function drawScore() {
+    context.font = '18px sans-serif';
+    context.fillStyle = '#ffffff';
+    context.fillText('Score: ' + score, 310, 25);
+}
 function blocklanes() {
     brickInterval = window.setInterval(() => {
-        let laneIndex = Math.floor(Math.random() * noOfLanes),
-            lane = lanes[laneIndex];
-        lanes[laneIndex].push({
-            x: lane[lane.length - 1] ? lane[lane.length - 1].x : laneIndex * 100 + 25,
-            brickLevel: ++latestBrickLevel
-        });
-        draw();
+        if (!pauseInterval) {
+            let laneIndex = Math.floor(Math.random() * noOfLanes),
+                lane = lanes[laneIndex];
+            lanes[laneIndex].unshift({
+                x: lane[lane.length - 1] ? lane[lane.length - 1].x : laneIndex * 100 + carMargin,
+                brickLevel: ++latestBrickLevel
+            });
+            draw();
+        }
     }, 1000);
 }
-
-document.querySelector('button').addEventListener('click', () => {
+function cancelInterval() {
     window.clearInterval(brickInterval);
+}
+document.querySelector('button').addEventListener('click', () => {
+    cancelInterval();
 });
